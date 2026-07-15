2bbdc8ad093d75225b15a4189e5b5328394f0d78
Tran Cong <tranconglk@gmail.com>
2026-07-15 17:41:56 +0700

    Release candidate v0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.trancong.dexworkspacemanager"
         minSdk = 28
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1-test"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
